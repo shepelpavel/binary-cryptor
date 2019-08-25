beb77d58345a612b3281e6c8cc097d04df1f6bbf
Shepel Pavel <shepelpav@gmail.com>
2019-08-25 03:04:01 +0300

    Добавление функции сдвига

--- script.js
+++ script.js
@@ -34,11 +34,36 @@ $( document ).ready(function() {
 		return res;
 	}
 
+	// Функция получения размера сдвига
+	// key - str origin
+	// val - str bin text
+	function inShiftVal(key, val) {
+		var shift = toBinStr(key).replace(/0/gi, '').length;
+		if (val.length > shift) {
+			var res = shift;
+		} else if (val.length < shift) {
+			if (shift % val.length != 0) {
+				var res = shift % val.length;
+			} else {
+				if (toBinStr(key.charAt(1)).replace(/0/gi, '').length < 8) {
+					var res = toBinStr(key.charAt(0)).replace(/0/gi, '').length;
+				} else {
+					var res = 7;
+				}
+			}
+		} else {
+			if (toBinStr(key.charAt(1)).replace(/0/gi, '').length < 8) {
+				var res = toBinStr(key.charAt(0)).replace(/0/gi, '').length;
+			} else {
+				var res = 7;
+			}
+		}
+		return res;
+	}
+
 	$('.key').focus();
 	$('body').on('keyup', '.key', function() {
-
 		var key = $('.key').val();
-
 		/* --- */
 		var key_bin_str = toBinStr(key);
 		var key_bin_arr = toBinArr(key);
@@ -50,7 +75,19 @@ $( document ).ready(function() {
 		$('.key-bin-str').val(key_bin_str);
 		$('.key-bin-arr').val(key_bin_arr);
 		/* --- */
+	});
+	$('body').on('keyup', '.in-step-0', function() {
+		var key = $('.key').val();
+		var in_val = $('.in-step-0').val();
+		var in_val_bin = toBinStr(in_val);
 
+		var in_sift = inShiftVal(key, in_val_bin);
 
+		var in_val_bin_first = in_val_bin.substr(0, in_val_bin.length - in_sift);
+		var in_val_bin_last = in_val_bin.substr(-in_sift);
+		var in_val_bin_res = in_val_bin_last + in_val_bin_first;
+
+		$('.test').val(in_sift);
+		$('.in-step-1').val(in_val_bin_res);
 	});
 });
