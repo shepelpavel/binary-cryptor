d09866c4a88d0e8a5dc5df1e5c654161f4d8987a
Shepel Pavel <shepelpav@gmail.com>
2019-08-26 00:02:50 +0300

    Добавление функции bin to text, доработки сдвига

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
 $( document ).ready(function() {
-
+	
 	//Функция добавления нулей в начало бинарного блока символа
-	//Принимает символ utf8
+	//Принимает символ utf16
+	// @char(string[1])
 	function missZero(char) {
 		var bin_convert = char.charCodeAt().toString(2);
 		var bin_length = bin_convert.length;
-		var need_zero = 8 - bin_length;
+		var need_zero = 16 - bin_length;
 		while (need_zero > 0) {
 			bin_convert = '0' + bin_convert;
 			need_zero--;
 		}
 		return bin_convert;
 	}
-
+	
 	// Функция формирования строки с бинарным кодом текста
-	// Принимает строку символов utf8
+	// Принимает строку символов utf16
+	// @str(string)
 	function toBinStr(str) {
 		var res = '';
 		for (var i = 0; i < str.length; i++) {
@@ -22,10 +24,11 @@ $( document ).ready(function() {
 		}
 		return res;
 	}
-
+	
 	// Функция формирования массива с бинарным кодом текста
 	// 1 ячейка - 1 символ
-	// Принимает строку символов utf8
+	// Принимает строку символов utf16
+	// @str(string)
 	function toBinArr(str) {
 		var res = [];
 		for (var i = 0; i < str.length; i++) {
@@ -33,7 +36,20 @@ $( document ).ready(function() {
 		}
 		return res;
 	}
-
+	
+	// Функция перевода бинарной строки в символьную
+	// @str(string)
+	function binToStr(str) {
+		str = str.replace(/\s+/g, '');
+		str = str.match(/.{1,16}/g).join(" ");
+		var newBinary = str.split(" ");
+		var binaryCode = [];
+		for (i = 0; i < newBinary.length; i++) {
+			binaryCode.push(String.fromCharCode(parseInt(newBinary[i], 2)));
+		}
+		return binaryCode.join("");
+	}
+	
 	// Функция получения размера сдвига
 	// key - str origin
 	// val - str bin text
@@ -45,49 +61,53 @@ $( document ).ready(function() {
 			if (shift % val.length != 0) {
 				var res = shift % val.length;
 			} else {
-				if (toBinStr(key.charAt(1)).replace(/0/gi, '').length < 8) {
+				if (toBinStr(key.charAt(1)).replace(/0/gi, '').length < 16) {
 					var res = toBinStr(key.charAt(0)).replace(/0/gi, '').length;
 				} else {
-					var res = 7;
+					var res = 13;
 				}
 			}
 		} else {
-			if (toBinStr(key.charAt(1)).replace(/0/gi, '').length < 8) {
+			if (toBinStr(key.charAt(1)).replace(/0/gi, '').length < 16) {
 				var res = toBinStr(key.charAt(0)).replace(/0/gi, '').length;
 			} else {
-				var res = 7;
+				var res = 13;
 			}
 		}
 		return res;
 	}
-
+	
+	function coding(key, val) {
+		var val_bin = toBinStr(val);
+		var sift = inShiftVal(key, val_bin);
+		var first = val_bin.substr(0, val_bin.length - sift);
+		var last = val_bin.substr(-sift);
+		var res = [
+			last + first,
+			'1',
+			'2',
+			'3'
+		];
+		return (res)
+	}
+	
+	
 	$('.key').focus();
 	$('body').on('keyup', '.key', function() {
 		var key = $('.key').val();
-		/* --- */
-		var key_bin_str = toBinStr(key);
-		var key_bin_arr = toBinArr(key);
-		var key_bin = '';
-		for (var i = 0; i < key.length; i++) {
-			key_bin += missZero(key[i]) + '(' + key[i].charCodeAt() + ')' + ' ';
-		}
-		$('.key-bin').val(key_bin);
-		$('.key-bin-str').val(key_bin_str);
-		$('.key-bin-arr').val(key_bin_arr);
-		/* --- */
+		var temp1 = toBinStr(key);
+		$('.temp1').val(temp1);
 	});
 	$('body').on('keyup', '.in-step-0', function() {
 		var key = $('.key').val();
 		var in_val = $('.in-step-0').val();
-		var in_val_bin = toBinStr(in_val);
-
-		var in_sift = inShiftVal(key, in_val_bin);
-
-		var in_val_bin_first = in_val_bin.substr(0, in_val_bin.length - in_sift);
-		var in_val_bin_last = in_val_bin.substr(-in_sift);
-		var in_val_bin_res = in_val_bin_last + in_val_bin_first;
-
-		$('.test').val(in_sift);
-		$('.in-step-1').val(in_val_bin_res);
+		
+		var step_1 = coding(key, in_val);
+		var temp2 = toBinStr(in_val);
+		var temp2shift = inShiftVal(key, temp2)
+		/*  tests  */
+		$('.temp2').val(temp2 + '('+ temp2shift +')');
+		$('.in-step-1').val(step_1[0]);
+		
 	});
 });
